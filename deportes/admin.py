397d5ba507d3129a466c815f6b745d915fe415c3
malvajohnnyalex <malvajohnnyalex@uss.edu.pe>
2026-05-24 01:25:28 -0500

    panel de administrador para Eventos y Cuota

--- deportes/admin.py
+++ deportes/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Evento, Cuota
 
-# Register your models here.
+# Esto permite que las cuotas se editen dentro de la misma pantalla del Evento
+class CuotaInline(admin.StackedInline):
+    model = Cuota
+    can_delete = False
+    verbose_name_plural = 'Cuotas del Partido'
+
+@admin.register(Evento)
+class EventoAdmin(admin.ModelAdmin):
+    # Columnas que se verán en la lista principal
+    list_display = ('equipo_local', 'equipo_visitante', 'fecha_hora', 'estado')
+    # Filtros laterales
+    list_filter = ('estado', 'fecha_hora')
+    # Barra de búsqueda
+    search_fields = ('equipo_local', 'equipo_visitante')
+    # Integra las cuotas en la vista del evento
+    inlines = [CuotaInline]
